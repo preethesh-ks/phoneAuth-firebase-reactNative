910e4a10a0c7ed68d89a48d7a85e4e1dd90afafc
Preethesh k.s <preetheshprezz@gmail.com>
2023-08-01 13:10:54 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,8 @@
 import * as firebase from "firebase";
 import "@firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDHIxHTAd01QdxyxO53luMQKupcOAcBmvg",
-  authDomain: "test-auth-cdbe2.firebaseapp.com",
-  projectId: "test-auth-cdbe2",
-  storageBucket: "test-auth-cdbe2.appspot.com",
-  messagingSenderId: "402026757869",
-  appId: "1:402026757869:web:5e31a47fd2b4add0fc56e4",
-};
+//firebaseconfig here 
+
+
 firebase.initializeApp(firebaseConfig);
 export default firebase;
